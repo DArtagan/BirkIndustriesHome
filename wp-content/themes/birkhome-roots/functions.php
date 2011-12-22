67173a6c4375d2ee12f95828294526e060e3a1a2
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-12-22 13:43:31 -0700

    Defined header image

---
+++ wp-content/themes/birkhome-roots/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+// Header Image Settings
+// http://codex.wordpress.org/Function_Reference/add_custom_image_header
+define('HEADER_TEXTCOLOR', '');
+define('NO_HEADER_TEXT', true);
+define('HEADER_IMAGE', get_stylesheet_directory_uri() . '/img/logo.png');
+define('HEADER_IMAGE_WIDTH', 225);
+define('HEADER_IMAGE_HEIGHT', 300);
+
+?>
