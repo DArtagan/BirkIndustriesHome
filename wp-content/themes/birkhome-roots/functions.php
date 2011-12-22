5065908d0a26baf0242153a2af888b662f8551bd
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2011-12-22 13:45:15 -0700

    Defined header image size

--- wp-content/themes/birkhome-roots/functions.php
+++ wp-content/themes/birkhome-roots/functions.php
@@ -5,7 +5,7 @@
 define('HEADER_TEXTCOLOR', '');
 define('NO_HEADER_TEXT', true);
 define('HEADER_IMAGE', get_stylesheet_directory_uri() . '/img/logo.png');
-define('HEADER_IMAGE_WIDTH', 225);
-define('HEADER_IMAGE_HEIGHT', 300);
+define('HEADER_IMAGE_WIDTH', auto);
+define('HEADER_IMAGE_HEIGHT', 75);
 
 ?>
